ec48731ef62f96095629819f71954e00e16f91fc
Jeroen van der Heijden <jeroen@cesbit.com>
2024-03-14 16:08:40 +0100

    py.11

--- setup.py
+++ setup.py
@@ -40,10 +40,10 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     packages=find_packages(),
     install_requires=[
